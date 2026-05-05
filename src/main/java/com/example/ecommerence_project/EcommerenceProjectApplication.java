a75819d5f9710b2a62e92b8fc9861f3208112605
abdallahjr111 <abdallah2303170@miuegypt.edu.eg>
2026-05-05 22:55:52 +0300

    .

---
+++ src/main/java/com/example/ecommerence_project/EcommerenceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerence_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerenceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerenceProjectApplication.class, args);
+    }
+
+}
