ac481d876296719c4b6cc8d57d2dcbc679b7cb30
Amr Mahmoud Heiba <amr2305049@miuegypt.edu.eg>
2026-05-07 20:12:09 +0300

    tazbetet el migrations

--- src/main/java/com/example/ecommerence_project/entity/User.java
+++ src/main/java/com/example/ecommerence_project/entity/User.java
@@ -1,4 +1,16 @@
 package com.example.ecommerence_project.entity;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "email") private String email;
+    @Column(name = "password") private String password;
+    @Column(name = "createdAt")private Date createdAt;
 }
